67a403012fb96ffab37bcfd3995d005f200eca04
DarianAlavi <darianthemonke@gmail.com>
2025-12-05 22:10:17 +0000

    Project 1 bru

---
+++ Projects/project1.py
@@ -0,0 +1,28 @@
+import sys
+import time
+
+def tuffthingyosammuzzyiscool(text, delay=0.05):
+
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+        
+name = input("What is your name? ")
+input()
+tuffthingyosammuzzyiscool(f"cool name {name} im a computer.")
+animal = input(" Whats your favorite animal? [Just the name] ")
+tuffthingyosammuzzyiscool(f"{animal}'s are cool! ")
+schoolfav = input("Whats your favorite school subject? ")
+tuffthingyosammuzzyiscool(f"{schoolfav} is pretty cool. ")
+age = input("How old are you? ")
+tuffthingyosammuzzyiscool(f"dang {age} is unc. ")
+sport = input("What sports do you play? ")
+tuffthingyosammuzzyiscool(f"{sport} is a cool sport. ")
+colorlike = input("Whats your favorite color? ")
+tuffthingyosammuzzyiscool(f"{colorlike} is a cool color! ")
+tuffthingyosammuzzyiscool("Hmmmmmmmmmmmmmm should I ask more questions???")
+tuffthingyosammuzzyiscool("IDK maaaaaaan")
+tuffthingyosammuzzyiscool("welp im gonna")
+goodday = input("How is your day? ")
+tuffthingyosammuzzyiscool()
